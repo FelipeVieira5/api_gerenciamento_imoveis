f44eff782f85556725f6ea44fe64f2351533cf76
FelipeVieira4 <felipevieira58238@gmail.com>
2024-03-31 00:09:22 -0300

    add: tipoImovel na api

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ meuServidor.use(espresso.json());
 const rotasEnderecos = require('./endereco/controladorEndereco');
 meuServidor.use(rotasEnderecos);
 
+const rotasTiposImoveis = require('./tipoImovel/controladorTipoImovel');
+meuServidor.use(rotasTiposImoveis);
+
 meuServidor.listen(4300, () => {
     console.log('Meu primeiro servidor na porta 4300.');
 });
